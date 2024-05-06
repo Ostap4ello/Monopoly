818bfbb29dec655a07d0defd4673601e01b3b1d8
Ostap4ello <okpeleh@gmail.com>
2024-05-06 11:01:04 +0200

    Added 'n filled data structures and added comments

--- main.c
+++ main.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include <time.h>
+#include <time.h>
 #include <unistd.h>
 #include <ncurses.h>
 
 
-#define sth 0
+
+#define MAX_NAME 100        // max. namelength
+#define MAX_PLAYERS 4       // max. player count
+#define NUM_SPACE_TYPES 6
+#define NUM_COLORS 8        // color count
 
 
-typedef struct _space {
 
-} SPACE;
-
-typedef struct _property {
-
-} PROPERTY;
-
-typedef struct _player {
-
-} PLAYER;
-
-typedef struct _flag {
-
-} FLAG;
-
+// Game states
 typedef enum _endpoint {
-    game_start,
-    game_process,
-    game_end,
+    game_start,             // game initialisation state
+    game_process,           // game process - play
+    game_end,               // game end - prompt
     eol
 } ENDPOINT;
 
+// Properties' color values
+typedef enum _color {
+    Brown,
+    Skyblue,
+    Purple,
+    Orange,
+    Red,
+    Yellow,
+    Green,
+    Blue
+} COLOR;
+
+// Space types
+typedef enum _space_type {
+    Property,
+    Start,
+    Free_parking,   // No action space
+    In_jail,
+    Go_to_jail,
+    Jail_pass
+} SPACE_TYPE;
 
 
+
+// Structure for properties' specs
+typedef struct _property {
+    char name[MAX_NAME + 1];
+    int price;
+    COLOR color;
+    int isOwned;        // -1 if not owned, playerIndex otherwise
+    int isMonopoly;     // 1 if is monopoly, 0 otherwise
+} PROPERTY;
+
+// Structure for spaces' specs
+typedef struct _space {
+    SPACE_TYPE type;        // space type on gameboard
+    PROPERTY *property;     // NULL if space is not a property, pointer to corresponding property otherwise
+} SPACE;
+
+// Structure for player information
+typedef struct _player {
+    int index;                      // playerIndex, index of player
+    int space_number;               // space number the player is currently standing on
+    int cash;                       // player's cash
+    int num_jail_pass;              // player's number of jail skip passes
+    int is_in_jail;                 // 1 if player is currently in jail, 0 otherwise
+    PROPERTY ** owned_properties;   // pointer to list of pointers to properties, owned by player
+    int num_properties;             // owned properties count
+} PLAYER;
+
+// Structure for flag info
+typedef struct _flag {
+    int isOn;           // 1 if flag is on, 0 otherwise
+    char * argument;    // pointer to argv argument if aviable, NULL otherwise
+} FLAG;
+
+
+// global variables definitions
 SPACE * gameboard = NULL;
 int space_count;
 
@@ -45,7 +91,11 @@ int player_count, playerIndex;
 
 ENDPOINT game_state;
 
+// get cl arguments and write into FLAG
 void getOptions(int argc, char * argv[]);
+
+// initialise players, gameboard, properties and global variables
+void initGame();
 
 void drawBoard();
 
@@ -61,7 +111,9 @@ void Game(){
         switch (game_state)
         {
         case game_start:
-            initGameboard();
+            initGame();
+
+            game_state = game_process;
             drawBoard();
             break;
 
